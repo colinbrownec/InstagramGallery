a3e99b2f3e01a36a6fcddfefa4d3472613a3d535
Colin Brown <colinbrownec@gmail.com>
2013-02-18 23:45:20 -0600

    Added pinch zoom
    Ready for panning

--- src/com/example/instagramgallery/ZoomImageView.java
+++ src/com/example/instagramgallery/ZoomImageView.java
@@ -2,79 +2,98 @@ package com.example.instagramgallery;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
 import android.util.AttributeSet;
-import android.util.Log;
+import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
-import android.view.ScaleGestureDetector.OnScaleGestureListener;
 import android.widget.ImageView;
 
 public class ZoomImageView extends ImageView {
-	private static float minZoom = 1f, maxZoom = 5f;
-	
+	private static float minZoom = 0.5f, maxZoom = 3f;
+
 	private float scale = 1f;
-	private float lastDistance = 0;
 	private ScaleGestureDetector detector;
 	
+	private int width, height, minSize;
+	private Matrix transform;
+
 	public ZoomImageView(Context context, AttributeSet set, int defStyle) {
 		super(context, set, defStyle);
 		setupPinchZoom();
 	}
-	
+
 	public ZoomImageView(Context context, AttributeSet set) {
 		super(context, set);
 		setupPinchZoom();
 	}
-	
+
 	public ZoomImageView(Context context) {
 		super(context);
 		setupPinchZoom();
 	}
-	
+
 	public void setupPinchZoom() {
-		detector = new ScaleGestureDetector(getContext(), new OnScaleGestureListener() {
-			@Override
-			public boolean onScale(ScaleGestureDetector detector) {
-				scale *= detector.getScaleFactor();
-				scale = Math.max(minZoom, Math.min(scale, maxZoom));
-				Log.i("crb", "scale = " + scale);
-				invalidate();
-				return true;
-			}
-
-			@Override
-			public boolean onScaleBegin(ScaleGestureDetector detector) {
-				if (lastDistance == 0) {
-					lastDistance = detector.getCurrentSpan();
-					
-				} else {
-					float delta = detector.getCurrentSpan() - lastDistance;
-					scale += Math.signum(delta) * scale * 0.01;
-					
-					Log.i("crb", "distance = " + delta);
-					Log.i("crb", "scale = " + scale);
+		// get the size of the screen
+		DisplayMetrics metrics = getContext().getResources().getDisplayMetrics();
+		width = metrics.widthPixels;
+		height = metrics.heightPixels;
 		
-					lastDistance = detector.getCurrentSpan();
-				}
+		// set the size of the picture to fit the smallest orientation of the screen
+		minSize = Math.min(width, height);
+		scale = (float) minSize / 612;
 		
-				return false;
-			}
-
-			@Override
-			public void onScaleEnd(ScaleGestureDetector detector) {
-			}
-		});
+		// adjust scaling limits if needed
+		if (scale > maxZoom)
+			maxZoom = scale;
+		if (scale < minZoom)
+			minZoom = scale;
+		
+		// create a transformation matrix and set the scale and translation based on the initial scale
+		transform = new Matrix();
+		transform.setScale(scale, scale);
+		transform.postTranslate(-306 * scale, -306 * scale);
+		
+		// set up a listener for pinch and zoom
+		detector = new ScaleGestureDetector(getContext(), new ScaleListener());
 	}
-	
+
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		detector.onTouchEvent(event);
 		return true;
 	}
-	
+
 	@Override
 	public void onDraw(Canvas canvas) {
-		canvas.scale(scale, scale);
-		super.onDraw(canvas);
+		BitmapDrawable draw = (BitmapDrawable) this.getDrawable();
+		if (draw != null) {		
+			// draw a bitmap with the desired scaling and translate the canvas to pan
+			canvas.translate(width / 2, height / 2);
+			canvas.drawBitmap(draw.getBitmap(), transform, null);
+		}
+	}
+	
+	/**
+	 * An extension of the android scaled gesture listener that implements adjusting the 
+	 * image scale based on how the user pinches the screen
+	 * @author Colin
+	 */
+	private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+		@Override
+		public boolean onScale(ScaleGestureDetector detector) {
+			// adjust our scaling based on the gesture
+			scale *= detector.getScaleFactor();
+			scale = Math.max(minZoom, Math.min(scale, maxZoom));
+			
+			// adjust matrix
+			transform.setScale(scale, scale);
+			transform.postTranslate(-306 * scale, -306 * scale);
+			
+			// tell the canvas to redraw
+			invalidate();
+			return true;
+		}
 	}
 }
